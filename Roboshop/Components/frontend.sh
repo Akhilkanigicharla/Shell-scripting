aae7cd3e1a1568c0d57ec98b0821742a27abb0fd
Akhilkanigicharla <kanigicharlaakhil9@gmail.com>
2021-06-01 18:37:42 +0530

    makefile making practise

--- Roboshop/Components/frontend.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo -e "\e[1m---------------------------------------"
-echo -e "Installing Nginx"
-echo -e "---------------------------------------\e[0m"
-
-
-#yum install nginx -y &>>/tmp/roboshop.log
-
---
+++ Roboshop/components/frontend.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo -e "\e[1m---------------------------------------------------"
+echo -e "Installing Nginx"
+echo -e "----------------------------------------------------\e[0m"
+
+
+#yum install nginx -y &>>/tmp/roboshop.log
+
