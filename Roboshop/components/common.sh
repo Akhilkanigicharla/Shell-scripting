6f4aae9d72cab5bd803ad49f49ca4bb321b7ae50
Akhilkanigicharla <kanigicharlaakhil9@gmail.com>
2021-06-01 21:07:38 +0530

    makefile making practise it m

--- Roboshop/components/common.sh
+++ Roboshop/components/common.sh
@@ -1,3 +1,13 @@
 HEAD() {
-  echo -e "\e[1m $1 \e[0m \t\t ... "
+  echo -n -e "\e[1m $1 \e[0m \t\t ... "
+}
+
+STAT() {
+  if [ $1 -eq 0 ]; then
+    echo -e "\e[1;32m done\e[0m"
+  else
+    echo -e "\e[1;32m fail\e[0m"
+    echo -e "\t \e[1;33m check the log file for more details ...log-File : /tmp/roboshop.log\e[0m"
+    exit 1
+  fi
 }
