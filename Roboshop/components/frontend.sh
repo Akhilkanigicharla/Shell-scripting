71cfb8295d8099b5e2f218442d049a9658de7a17
Akhilkanigicharla <kanigicharlaakhil9@gmail.com>
2021-06-02 13:04:41 +0530

    makefile making practise it m

--- Roboshop/components/frontend.sh
+++ Roboshop/components/frontend.sh
@@ -7,5 +7,11 @@ HEAD "Installing Nginx"
 yum install nginx -y &>>/tmp/roboshop.log
 STAT $?
 
+HEAD "Start Nginx"
+systemctl start nginx &>>/tmp/roboshop.log
+systemctl enable ngnix &>>/tmp/roboshop.log
+STAT $?
+
+
 
 
