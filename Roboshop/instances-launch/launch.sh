76b98b55bc31120a39161a2e4638fd5ea106ebcd
Akhilkanigicharla <kanigicharlaakhil9@gmail.com>
2021-06-02 17:43:47 +0530

    makefile making practise it m

---
+++ Roboshop/instances-launch/launch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+COMPONENT=$1
+
+if [ -z "${COMPONENT}" ]; then
+   echo "Component Input is needed"
+   exit 1
+fi
+
+LID=lt-0d8e2147e14cf14d9
+LVER=1
+
+ aws ec2 run-instances  --launch-template  LaunchTemplateId=${LID},Version=${LVER} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=COMPONENT}]"
