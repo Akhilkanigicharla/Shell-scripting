643952cc2481383e9eb3667a81e0e1e8b1a787b0
Akhilkanigicharla <kanigicharlaakhil9@gmail.com>
2021-06-02 17:47:18 +0530

    makefile making practise it m

--- Roboshop/instances-launch/launch.sh
+++ Roboshop/instances-launch/launch.sh
@@ -10,4 +10,4 @@ fi
 LID=lt-0d8e2147e14cf14d9
 LVER=1
 
- aws ec2 run-instances  --launch-template  LaunchTemplateId=${LID},Version=${LVER} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=COMPONENT}]"
+ aws ec2 run-instances  --launch-template  LaunchTemplateId=${LID},Version=${LVER} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"
