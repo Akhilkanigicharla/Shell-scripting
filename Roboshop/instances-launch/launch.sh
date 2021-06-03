e5de9a175d731387f99df624bbc3b84577129b8f
Akhilkanigicharla <kanigicharlaakhil9@gmail.com>
2021-06-03 17:29:39 +0530

    makefile making practise it m

--- Roboshop/instances-launch/launch.sh
+++ Roboshop/instances-launch/launch.sh
@@ -2,12 +2,35 @@
 
 COMPONENT=$1
 
+## -z validates the variable empty , true if it is empty.
 if [ -z "${COMPONENT}" ]; then
-   echo "Component Input is needed"
-   exit 1
+  echo "Component Input is Needed"
+  exit 1
 fi
 
-LID=lt-0d8e2147e14cf14d9
+LID=lt-090f6832097248547
 LVER=1
 
- aws ec2 run-instances  --launch-template  LaunchTemplateId=${LID},Version=${LVER} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"
+## Validate If Instance is already there
+
+DNS_UPDATE() {
+  PRIVATEIP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}"  | jq .Reservations[].Instances[].PrivateIpAddress | xargs -n1)
+  sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${PRIVATEIP}/" record.json >/tmp/record.json
+  aws route53 change-resource-record-sets --hosted-zone-id Z04221902CM9ZT2GHM1NW --change-batch file:///tmp/record.json | jq
+}
+
+INSTANCE_STATE=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}"  | jq .Reservations[].Instances[].State.Name | xargs -n1)
+if [ "${INSTANCE_STATE}" = "running" ]; then
+  echo "Instance already exists!!"
+  DNS_UPDATE
+  exit 0
+fi
+
+if [ "${INSTANCE_STATE}" = "stopped" ]; then
+  echo "Instance already exists!!"
+  exit 0
+fi
+
+aws ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq
+sleep 30
+DNS_UPDATE
