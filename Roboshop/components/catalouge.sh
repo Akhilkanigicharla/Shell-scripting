13e1b21249dc6cda6d1d9904c195294d78d86e60
Akhilkanigicharla <kanigicharlaakhil9@gmail.com>
2021-06-03 18:47:11 +0530

    makefile making practise it m

--- Roboshop/components/catalouge.sh
+++ Roboshop/components/catalouge.sh
@@ -1,29 +1,44 @@
 #!/bin/bash
 
-source /components/common.sh
+source components/common.sh
 rm -f /tmp/roboshop.log
 set-hostname catalogue
 
-HEAD "Install nodejs"
+
+HEAD "Install NodeJS\t\t\t"
 yum install nodejs make gcc-c++ -y &>>/tmp/roboshop.log
 STAT $?
 
-HEAD "ADD roboshop user"
-useradd roboshop &>>/tmp/roboshop.log
-STAT $?
+HEAD "Add RoboShop App User\t\t"
+id roboshop &>>/tmp/roboshop.log
+if [ $? -eq 0 ]; then
+  echo User is already there, So Skipping the User creation &>>/tmp/roboshop.log
+  STAT $?
+else
+  useradd roboshop &>>/tmp/roboshop.log
+  STAT $?
+fi
 
-HEAD "Download app from git hub"
+HEAD "Download App from GitHub\t"
 curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>/tmp/roboshop.log
 STAT $?
 
-HEAD "Extract the Downloaded archive"
-cd /home/roboshop && unzip /tmp/catalogue.zip &>>/tmp/roboshop.log &&mv catalogue-main catalogue
+HEAD "Extract the Downloaded Archive"
+cd /home/roboshop && rm -rf catalogue && unzip /tmp/catalogue.zip &>>/tmp/roboshop.log && mv catalogue-main catalogue
 STAT $?
 
-HEAD "Install nodejs dependences"
-cd /home/roboshop/catalouge && npm install &>>/tmp/roboshop.log
+HEAD "Install NodeJS Dependencies\t"
+cd /home/roboshop/catalogue && npm install --unsafe-perm &>>/tmp/roboshop.log
 STAT $?
 
+HEAD "Fix Permissions to App Content"
+chown roboshop:roboshop /home/roboshop -R
+STAT $?
 
+HEAD "Setup SystemD Service\t\t"
+sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/roboshop/catalogue/systemd.service  && mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
+STAT $?
 
-
+HEAD "Start Catalogue Service\t"
+systemctl daemon-reload && systemctl enable catalogue &>>/tmp/roboshop.log && systemctl restart catalogue &>>/tmp/roboshop.log
+STAT $?
